f9c9fb959346d8eb9c1e6636540a084ba2fa5e7b
zafarbek tolanboyev <zafarvektolanboyev618@gmail.com>
2024-03-31 06:16:15 +0500

    htmljsadd

---
+++ app.js
@@ -0,0 +1,69 @@
+// class car {
+//     constructor(brand, year){
+//         this.brand = brand;
+//         this.year = year;
+//     }
+// }
+// let car1 = new car ("nexia", 2000);
+// let car2 = new car("damas", 1995);
+// let car3 = new car("gentra", 2005);
+// let car4 = new car("tracker", 2015);
+
+// console.log(car1);
+// console.log(car2);
+// console.log(car3);
+// console.log(car4);
+
+
+
+
+// class ElectrickCar {
+//     constructor(brand, year, batterylife, color, price){
+//         this.brand = brand;
+//         this.year = year;
+//         this.batterylife = batterylife;
+//         this.color = color;
+//         this.price = price;
+//     }
+// }
+// let car1 = new ElectrickCar("nexia", 2000, gray, 150000000);
+// let car2 = new ElectrickCar("damas", 1995, white, 90000000);
+// let car3 = new ElectrickCar("gentra", 2005, 100000000);
+// let car4 = new ElectrickCar("tracker", 2015,200000000);
+// let electric1 = new ElectrickCar("tesla", 20015, white, 40000000, battery);
+// let electric2= new ElectrickCar("BYD", 2023, black, 20000000, battery); 
+// console.log(car1);
+// console.log(car2);
+// console.log(car3);
+// console.log(car4);
+// console.log(electric1);
+// console.log(electric2)
+
+
+
+// class Car {
+//     constructor(brand, year) {
+//         this.brand = brand;
+//         this.year = year;
+//     }
+
+//     displayInfo() {
+//         console.log(`Brand: ${this.brand}, Year: ${this.year}`);
+//     }
+// }
+// class ElectricCar extends Car {
+//     constructor(brand, year, batteryLife) {
+//         super(brand, year);
+//         this.batteryLife = batteryLife;
+//     }
+// }
+// let car1 = new Car("Tayota", 2020);
+// let car2 = new Car("BMW", 2018);
+// let car3 = new Car("Mercedes", 2022);
+// car1.displayInfo();
+// car2.displayInfo();
+// car3.displayInfo();
+// let electricCar = new ElectricCar("Tesla", 2023, "300 miles");
+// electricCar.displayInfo(); 
+
+
